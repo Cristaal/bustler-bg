20ae9b666c2c1b403152092fd476b6873d2ff5f5
James <jameskirkmaynard@gmail.com>
2015-03-02 12:09:36 -0800

    Hard code images into template and store files.

--- store.js
+++ store.js
@@ -2,25 +2,29 @@ var posts = [
   {
     id: '1',
     title: "Goldberg traps woman under laptop",
-    body: "Eek!"
+    body: "Eek!",
+    img: "src='http://www.newyorker.com/wp-content/uploads/2013/09/130923_r23961-1200.jpg'"
   },
-  
+
   {
     id: '2',
     title: "Feminism hates Bryan Goldberg",
-    body: "You know it."
+    body: "You know it.",
+    img: "src='http://b-i.forbesimg.com/jeffbercovici/files/2013/08/300px-USpatent131840_18722.gif'"
   },
 
   {
     id: '3',
     title: "Why the 'Y' in Bryan Makes Goldberg A Fool",
-    body: "and not Bryan Cranston"
+    body: "and not Bryan Cranston",
+    img: "src='http://www.sunshineanddesign.com/wp-content/uploads/2013/08/bryan-cranston-cardigan-nip-gq-2010-5501.jpg'"
   },
 
   {
     id: '4',
     title: '77 Reasons This Corgy Is Wearing a Sweater',
-    body: '#1: Some crazy mamma wants a boy-child.'
+    body: '#1: Some crazy mamma wants a boy-child.',
+    img: "src='http://www.unbelievab.ly/wp-content/uploads/2014/12/f52371492f0be2a72be0131f3d8c553e.jpg'"
   }
 
 
